db7ce4cf2814ec78d274b5e7283bb6e39321505d
EVANGELISTA-RIBEIRO <joaovictor.setembro@gmail.com>
2024-12-19 13:25:27 -0300

    refactor: Adjust the path to the form1.html page in views.py

--- LogiStream/views.py
+++ LogiStream/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def form1(request):
     """Function for render form1."""
 
-    return render(request, 'LogiStream/form1.html')
+    return render(request, 'LogiStream/view/form1.html')
